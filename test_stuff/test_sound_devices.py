915c2611401fc4d3af3c9cf55f1328aca56093b3
Ben Czech <benczech212@gmail.com>
2025-08-09 13:28:28 -0400

    cleaning branch

--- test_stuff/test_sound_devices.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-
-def list_input_devices():
-    devices = sd.query_devices()
-    for idx, device in enumerate(devices):
-        if device['max_input_channels'] > 0:
-            print(f"{idx}: {device['name']} (Input channels: {device['max_input_channels']})")
-
-list_input_devices()
